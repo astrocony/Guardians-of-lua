5b9a3feb31ff6d9ce4e11cf2c7f5d9863ce55594
Cony Lepe <astrocony@MacBook-Pro-de-Cony.local>
2025-02-16 19:55:02 +0100

    cambiando a varios mensajes

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-function cambiarMensaje() {
-    const mensajeElemento = document.getElementById('mensaje');
-    mensajeElemento.innerText = "✨ ¡Lua te da la bienvenida, Guardián! ✨";
-    mensajeElemento.style.color = "#7b1fa2";
-    mensajeElemento.style.fontSize = "22px";
-  }
+// Array de mensajes posibles 
+
+const mensajes = [ 
+    "¿Estás perdido? Pronto encontraras la Luz",
+    "Una estrella hay al final del tunel esperandote",
+    "Sigue creyendo, estás muy cerca",
+    "Necesitas descansar un poco y todo te encontrará",
+    "¡Una estrella te encontrará! "
+];
+
+// indice para recorrer los mensajes
+
+let indiceMensaje=0;
+
+function cambiarMensaje(){
+  const mensajeElemento = document.getElementById('mensaje');
+  mensajeElemento.innerText = mensajes[indiceMensaje];
+  mensajeElemento.style.color = "#7b1fa2";
+  mensajeElemento.style.fontSize = "22px";
+
+  // Avanza al siguiente mensaje, vuelve al primero al terminar
+  indiceMensaje = (indiceMensaje + 1) % mensajes.length;
+
+}
+
+
   

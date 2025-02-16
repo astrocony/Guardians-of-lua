d675392656c6dd51ecc984ee74897fa808383b0e
Cony Lepe <astrocony@MacBook-Pro-de-Cony.local>
2025-02-16 19:09:25 +0100

    Corrigiendo imagen y boton

--- scipt.js
+++ scipt.js
@@ -1,13 +1,7 @@
 function cambiarMensaje() {
-    const mensajes = [
-      "🌙 Bienvenido, Guardián de Lua.",
-      "✨ Tu misión es proteger el conocimiento del universo.",
-      "⭐ Lua cree en ti, sigue tu intuición.",
-      "💫 El viaje apenas comienza, prepárate para la aventura."
-    ];
-  
-    const mensajeElemento = document.getElementById("mensaje");
-    const indice = Math.floor(Math.random() * mensajes.length);
-    mensajeElemento.textContent = mensajes[indice];
+    const mensajeElemento = document.getElementById('mensaje');
+    mensajeElemento.innerText = "✨ ¡Lua te da la bienvenida, Guardián! ✨";
+    mensajeElemento.style.color = "#7b1fa2";
+    mensajeElemento.style.fontSize = "22px";
   }
   

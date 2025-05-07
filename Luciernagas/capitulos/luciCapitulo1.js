3807bf2e51dd1769e1c8b579eedcb5e7ccacec89
Cony Lepe <astrocony@MacBook-Pro-de-Cony.local>
2025-05-07 03:34:52 +0200

    Luciernagas capitulo uno full screen prueba dos

--- Luciernagas/capitulos/luciCapitulo1.js
+++ Luciernagas/capitulos/luciCapitulo1.js
@@ -1,10 +1,22 @@
 
 document.getElementById('fullscreen-btn').addEventListener('click', function() {
+    const elem = document.documentElement; // Toma todo el documento
+
     if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
     } else {
         if (document.exitFullscreen) {
             document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
         }
     }
 });

1d024908ba2bc51ecb8987b4399210a7098bf140
Cony Lepe <astrocony@MacBook-Pro-de-Cony.local>
2025-05-07 03:22:24 +0200

    Luciernagas capitulo uno full screen

---
+++ Luciernagas/capitulos/luciCapitulo1.js
@@ -0,0 +1,10 @@
+
+document.getElementById('fullscreen-btn').addEventListener('click', function() {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+    }
+});
